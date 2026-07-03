c525f4273877a05554ddd8de50b50980ddfd36c2
Divya <divya-divyanshu>
2026-07-03 17:14:02 +0530

    change in 2nd line in master

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Git kehta hai, Hello Dunia, Kaise ho")
-print("......To dunia kahti hai")
+print("To dunia kahti hai")
 print("Sab mje me bhai, apna batao??")
 print("Git kehta hai, kya baatu apna hal ye ai ki wjh se behal hi chal rha hai")
